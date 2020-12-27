ccaf76d800b984b2578cbcba6f61bf7a3be0a67b
jhyounyaho <https://github.com/jhyounyaho>
2020-12-27 12:40:05 +0900

    class형 컴포넌트 > 함수형 컴포넌트 변경, 플레이어 차례 설정

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -6,15 +6,18 @@ class Board extends Component {
     super(props);
     this.state = {
       squares: new Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
-    //const squares = this.state.squares.slice();
-    // 불변성
+    // 불변성을 위해 spread 
     const squares = [...this.state.squares];
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
   
   renderSquare(i) {
@@ -27,7 +30,7 @@ class Board extends Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     return (
       <div>
         <div className='status'>{status}</div>
